0e7fb523ae2d7bbcfd397d59e6ce0797d62b83ed
Daniel Perry <dperry@danieltperry.me>
2023-03-16 14:56:00 -0400

    TEMP: openmw: 0.47.0 -> 48-rc8

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -36003,7 +36003,7 @@ with pkgs;
   openloco = pkgsi686Linux.callPackage ../games/openloco { };
 
   openmw = libsForQt5.callPackage ../games/openmw {
-    inherit (darwin.apple_sdk.frameworks) VideoDecodeAcceleration;
+    inherit (darwin.apple_sdk.frameworks) VideoDecodeAcceleration CoreMedia VideoToolbox;
   };
 
   openmw-tes3mp = libsForQt5.callPackage ../games/openmw/tes3mp.nix { };
